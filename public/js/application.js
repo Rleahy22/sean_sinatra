66ba532b4f6d71d6e3ede4f10465a7114a3448da
Ryan Leahy <ryanleahy90@gmail.com>
2014-02-17 15:57:53 -0600

    chagned tournament to dynamically build tournament size based on number of seeds

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var seeds = [];
 
   function appendVideo() {
     var videoDiv = '<div class="player"><button id="close">X</button></div><iframe id="index-vid" width="700" height="394" src="//www.youtube.com/embed/PCy4f99hxEc?autoplay=1" frameborder="0" allowfullscreen></iframe>';
@@ -25,21 +26,78 @@ $(document).ready(function() {
     removeVideo();
   });
 
-  $('#seed').on('click', function() {
-    for (var i = 0; i < 16; i++) {
-      var name = prompt("Seed" + (i + 1));
-      if (name != "" && name === null) {
-        break;
-      } else {
-        $('#team' + (i + 1)).html(name);
-      }
+  $('#seed').on('keyup', function(e) {
+    if (e.which == 13 || e.keyCode == 13) {
+      addSeed();
     }
   });
 
-  $('.round').on('click', '.team', function() {
+  function addSeed() {
+    var seed = $('#seed').val();
+    seeds.push(seed);
+
+    $('#list').show();
+    $('.bracket').hide();
+    $('#list').append('<li class="idea">' + seed + '</li>');
+    $('#seed').val('');
+    $('#seed').focus();
+  }
+
+  function bracketNumber(numSeeds) {
+    $('#list').hide();
+    $('#bracket' + numSeeds).show();
+  }
+
+  $('#add').on('click', function() {
+    addSeed();
+  });
+
+  $('#begin').on('click', function() {
+    var numSeeds = seeds.length;
+
+    if (numSeeds < 4) {
+      alert("Please enter at least 4 ideas to begin a tournament.");
+      $('#seed').focus();
+    } else {
+
+        if (numSeeds <= 8 && numSeeds > 4) { 
+          for (var i = 1; seeds.length < 8; i += 2) {
+            seeds.splice(i, 0, "(bye)");
+          }
+        } else if (numSeeds <= 16 && numSeeds > 4) {
+          for (var i =1; seeds.length < 16; i +=2) {
+            seeds.splice(i, 0, "(bye)");
+          }
+        }
+
+      numSeeds = (Math.ceil(numSeeds / 4)) + 1;
+      numSeeds = Math.pow(2, numSeeds);
+      console.log(numSeeds);
+      bracketNumber(numSeeds);
+
+    }
+
+    for (var seed in seeds) {
+      var index = parseInt(seed) + 1;
+      $('.team' + index).html(seeds[seed]).effect("highlight", {}, 750);
+    }
+
+    for (var j = 1; j < 17; j++) {
+      var team = $('.team' + j);
+      var otherTeam = $('.team' + (j - 1)).html();
+
+      if (team.html() === "(bye)") {
+        var matchup = team.parent().attr('class').substring(9);
+        $('.w' + matchup).html(otherTeam);
+      }
+    }
+
+  });
+
+
+  $('#tournament').on('click', '.team', function() {
     var winner = $(this).html();
-    var matchup = $(this).parent().attr('id').substring(1);
-    console.log(matchup);
-    $('#w' + matchup).html(winner);
+    var matchup = $(this).parent().attr('class').substring(9);
+    $('.w' + matchup).html(winner).effect("highlight", {}, 750);
   })
 });
